dae4ecca2d35dc1921e11f776dd8fdf56ca4362d
djshinnn <tls980824@naver.com>
2023-03-02 17:56:14 +0900

    error state context로 위치 변경

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -5,8 +5,7 @@ import { FormContext } from "../components/SimpleForm";
 interface UseInputProps extends Pick<InputProps, "source" | "validate"> {}
 
 function useInput(props: UseInputProps) {
-  const [error, setError] = useState(true);
-  const { setValues, values } = useContext(FormContext);
+  const { setValues, values, setError, error } = useContext(FormContext);
   const onChange = useCallback(
     (v: string | number) => {
       setValues({
